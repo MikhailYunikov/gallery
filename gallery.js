6b14ff32e95191596b7b1914142629048ee7c96b
Mikhail Yunikov <mikhail.yunikov@gmail.com>
2022-02-06 20:18:47 +0400

    add gallery folder with *.png, add smooth arrow brightness on mouse move

--- gallery.js
+++ gallery.js
@@ -1,15 +1,20 @@
 //let f
 let ynm_Parameters = {
     stringArrayOfImageNames: ["pic1.png", "pic2.png", "pic3.png", "pic4.png", "pic5.png", "pic6.png"],
-    imageCurrentNumber: 0
+    imageCurrentNumber: 5
 
 };
 const ynm_Constants = {
     
-    idRightArrowContainerWidth : 100
+    arrowContainerWidth : 100,
+    arrowOpacityMax: 0.7,
+    arrowOpacityMin: 0.1,
+    arrowOpacityDefault: 0
 
 };
 document.getElementById("idImageControlGridContainer").addEventListener("mouseup", ynm_showNextString);
+document.getElementById("idImageControlGridContainer").addEventListener("mousemove", ynm_arrowImgOpacityControl);
+document.getElementById("idImageControlGridContainer").addEventListener("mouseout", ynm_arrowImgSetOpacityToDefault);
 //document.getElementById("idLeftArrowContainer").addEventListener("mouseup", ynm_showNextString);
 //document.getElementById("idRightArrowContainer").addEventListener("mouseup", ynm_showNextString);
 document.addEventListener("keyup", ynm_showNextString);
@@ -29,12 +34,12 @@ function ynm_showNextString(e) {
     //
     if( e.type === "mouseup"){
         console.log(e.x);
-        if ( (Number(e.x) > Number(tmpObj.left)) && (Number(e.x) < (Number(tmpObj.left)+ Number(ynm_Constants.idRightArrowContainerWidth))) ){
+        if ( (Number(e.x) > Number(tmpObj.left)) && (Number(e.x) < (Number(tmpObj.left)+ Number(ynm_Constants.arrowContainerWidth))) ){
             console.log("get left image");
             if (ynm_Parameters.imageCurrentNumber <= 0) ynm_Parameters.imageCurrentNumber = ynm_Parameters.stringArrayOfImageNames.length - 1;
             else ynm_Parameters.imageCurrentNumber -= 1;
             document.getElementById("idMasterpiece").src = "./gallery/" + ynm_Parameters.stringArrayOfImageNames[ynm_Parameters.imageCurrentNumber];
-        } else if ((Number(e.x) < Number(tmpObj.right)) && (Number(e.x) > (Number(tmpObj.right) - Number(ynm_Constants.idRightArrowContainerWidth))) ){
+        } else if ((Number(e.x) < Number(tmpObj.right)) && (Number(e.x) > (Number(tmpObj.right) - Number(ynm_Constants.arrowContainerWidth))) ){
 
             if (ynm_Parameters.imageCurrentNumber >= ynm_Parameters.stringArrayOfImageNames.length - 1) ynm_Parameters.imageCurrentNumber = 0;
             else ynm_Parameters.imageCurrentNumber += 1;
@@ -83,4 +88,62 @@ function ynm_showNextString(e) {
     }
     document.getElementById("idMasterpiece").src = "./gallery/" + ynmParameters.stringArrayOfImageNames[++ynmParameters.imageNumber];
     */
+}
+
+function ynm_arrowImgOpacityControl(e){
+    let tmpObj = document.getElementById("idImageControlGridContainer").getBoundingClientRect();
+    //console.log(e.x);
+
+    if ( (Number(e.x) > Number(tmpObj.left)) && (Number(e.x) < (Number(tmpObj.left)+ Number(ynm_Constants.arrowContainerWidth))) ){
+        //opacity left arrow Max%, right Arrow should be Min%
+        console.log("opacity left arrow 0%");
+        document.getElementById("idLeftArrowContainer").style.opacity = String(ynm_Constants.arrowOpacityMax);//"0.8";
+        document.getElementById("idRightArrowContainer").style.opacity = String(ynm_Constants.arrowOpacityMin);//"0.1";
+        /*
+        if (ynm_Parameters.imageCurrentNumber <= 0) ynm_Parameters.imageCurrentNumber = ynm_Parameters.stringArrayOfImageNames.length - 1;
+        else ynm_Parameters.imageCurrentNumber -= 1;
+        document.getElementById("idMasterpiece").src = "./gallery/" + ynm_Parameters.stringArrayOfImageNames[ynm_Parameters.imageCurrentNumber];
+        */
+    } else if ((Number(e.x) < Number(tmpObj.right)) && (Number(e.x) > (Number(tmpObj.right) - Number(ynm_Constants.arrowContainerWidth))) ){
+        //opacity right arrow MAx%, left Arrow should be Min%
+        console.log("opacity Right arrow 0%");
+        document.getElementById("idLeftArrowContainer").style.opacity = String(ynm_Constants.arrowOpacityMin);//"0.8";
+        document.getElementById("idRightArrowContainer").style.opacity = String(ynm_Constants.arrowOpacityMax);//"0.1";
+
+        /*
+        if (ynm_Parameters.imageCurrentNumber >= ynm_Parameters.stringArrayOfImageNames.length - 1) ynm_Parameters.imageCurrentNumber = 0;
+        else ynm_Parameters.imageCurrentNumber += 1;
+        document.getElementById("idMasterpiece").src = "./gallery/" + ynm_Parameters.stringArrayOfImageNames[ynm_Parameters.imageCurrentNumber];
+        console.log("get right image");
+        */
+    }else{
+        //left/right arrow opacity should be computed
+        //get current opacity
+        let tmpLeftX = Number(tmpObj.left) + Number(ynm_Constants.arrowContainerWidth);
+        let tmpRightX  = Number(tmpObj.right) - Number(ynm_Constants.arrowContainerWidth);
+        let tmpImageContainerWidth = tmpRightX - tmpLeftX;
+        let tmpMouseScaledX = Number(e.x) - tmpLeftX;
+        let tmpRightImgOpacity = ((ynm_Constants.arrowOpacityMax - ynm_Constants.arrowOpacityMin) * (tmpMouseScaledX/tmpImageContainerWidth) + ynm_Constants.arrowOpacityMin).toPrecision(3);
+        
+        if(tmpRightImgOpacity > ynm_Constants.arrowOpacityMax)
+            tmpRightImgOpacity = ynm_Constants.arrowOpacityMax;
+
+        let tmpLeftImgOpacity = (ynm_Constants.arrowOpacityMax - tmpRightImgOpacity) + ynm_Constants.arrowOpacityMin;
+        //set opacity
+        document.getElementById("idLeftArrowContainer").style.opacity = String(tmpLeftImgOpacity);//"0.8";
+        document.getElementById("idRightArrowContainer").style.opacity = String(tmpRightImgOpacity);//"0.1";
+    
+        console.log("variable opacity for both arrows");
+        
+        //fuck off
+    }
+    //console.log("mouseX =" + String(e.x), " mouseY =" + String(e.y));
+
+}
+
+function ynm_arrowImgSetOpacityToDefault(){
+    //both arrow opacity should be default
+    console.log("default opacity");
+    document.getElementById("idLeftArrowContainer").style.opacity = String(ynm_Constants.arrowOpacityDefault);//"0.8";
+    document.getElementById("idRightArrowContainer").style.opacity = String(ynm_Constants.arrowOpacityDefault);//"0.1";
 }
